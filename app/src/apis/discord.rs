e951c274dd825320dd9a8a897f6dff96a873eb4a
Vlad Pronsky <v.pronsky@gmail.com>
2025-08-31 22:59:42 +0300

    feat: better discord error message #2

--- app/src/apis/discord.rs
+++ app/src/apis/discord.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use axum::extract::{Path, Query};
-use badgelib::Badge;
+use badgelib::{Badge, Color};
 use cached::proc_macro::cached;
 use serde::{Deserialize, Serialize};
 
@@ -9,18 +9,30 @@ use super::get_client;
 use crate::server::{BadgeRep, Res};
 
 #[derive(Debug, Clone)]
-struct Data {
-  members: u64,
+pub enum Data {
+  Members(u64),
+  Message(String),
 }
 
 #[cached(time = 60, result = true)]
 async fn get_data(name: String) -> Res<Data> {
   let url = format!("https://discord.com/api/v6/guilds/{name}/widget.json");
-  let rep = get_client().get(&url).send().await?.error_for_status()?;
+  let rep = get_client().get(&url).send().await?;
+
+  let status = rep.status();
   let dat = rep.json::<serde_json::Value>().await?;
 
-  let members = dat["presence_count"].as_u64().unwrap_or(0);
-  Ok(Data { members })
+  match status.as_u16() {
+    200 => {
+      let members = dat["presence_count"].as_u64().unwrap_or(0);
+      Ok(Data::Members(members))
+    }
+    403 | 404 => {
+      let msg = dat["message"].as_str().unwrap_or("unknown error").to_lowercase();
+      Ok(Data::Message(msg))
+    }
+    _ => Err(anyhow::anyhow!("Unexpected response code: {}", status)),
+  }
 }
 
 #[derive(Debug, Deserialize, Serialize, strum::EnumIter, strum::Display)]
@@ -34,6 +46,8 @@ pub async fn handler(
   Query(badge): Query<Badge>,
 ) -> BadgeRep {
   let rs = get_data(name).await?;
-  let value = format!("{} online", rs.members);
-  Ok(badge.label("discord").value(&value))
+  Ok(match rs {
+    Data::Members(m) => badge.label("discord").value(&format!("{} online", m)),
+    Data::Message(m) => badge.label("discord").value_color(Color::Red).value(&m),
+  })
 }
